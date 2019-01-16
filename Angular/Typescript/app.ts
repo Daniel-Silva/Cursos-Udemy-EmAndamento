ae0a9f958fb376205d47315364818c5fb38f8462
Daniel Silva <dani3l.dxp@gmail.com>
2019-01-16 20:11:29 -0200

    Iniciando conceitos de Typescript

---
+++ Angular/Typescript/app.ts
@@ -0,0 +1,6 @@
+let msg: string = "Daniel Silva";
+console.log(msg);
+
+let episode: number = 4;
+console.log("This is episode "+ 4);
+episode += 1;
---
+++ Angular/Typescript/dist/app.js
@@ -0,0 +1,6 @@
+var msg = "Daniel Silva";
+console.log(msg);
+var episode = 4;
+console.log("This is episode " + episode);
+episode += 1;
+console.log("Next episode " + episode);
