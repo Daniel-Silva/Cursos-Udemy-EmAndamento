65670996dd395ef2d125c7f4384a9fc52058757c
Daniel Silva <dani3l.dxp@gmail.com>
2019-01-19 12:15:26 -0200

    Entendendo a estrutura de uma aplicação, configuração do Bootstrap e gerando primeiro component

---
+++ Angular/Angular4/jedi-academy/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+// tslint:disable-next-line: deprecation
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
