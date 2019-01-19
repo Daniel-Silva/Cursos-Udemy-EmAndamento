2de8ad18c03a342480030009fddc9e84358a6826
Daniel Silva <dani3l.dxp@gmail.com>
2019-01-19 16:20:55 -0200

    Conhecendo Diretivas e Eventos

--- Angular/Angular4/jedi-academy/src/app/app.component.ts
+++ Angular/Angular4/jedi-academy/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Studant} from './student/student.component';
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Daniel Silva';
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'Han Solo', isJedi: false}
+  ];
 }
