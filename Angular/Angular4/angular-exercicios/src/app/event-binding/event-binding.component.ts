0dcf6f4d251491adf0b7e1254749acea2df9afea
Daniel Silva <dani3l.dxp@gmail.com>
2019-01-23 16:47:27 -0200

    Treinando Property Binding, interpolação Class e Style Bbinding

---
+++ Angular/Angular4/angular-exercicios/src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ae-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
